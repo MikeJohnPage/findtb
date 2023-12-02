3925c7d1e7091b14f3454659d60f10700041096b
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-12-02 10:33:30 +0100

    Add notifications data

--- data-raw/notifications.R
+++ data-raw/notifications.R
@@ -1,1 +1,26 @@
-readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=notifications")
+raw <- readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=notifications")
+
+raw_subset <- raw |> dplyr::select(
+  tidyselect::all_of(
+    c(
+      "country",
+      "country_code" = "iso3",
+      "g_whoregion",
+      "year",
+      "conf_rrmdr_tx",
+      "conf_xdr_tx",
+      "new_clindx",
+      "ret_rel_labconf",
+      "c_newinc",
+      "new_labconf"
+    )
+  )
+)
+
+notifications <- raw_subset |>
+  tidyr::pivot_longer(
+    cols = -c(country, country_code, g_whoregion, year),
+    names_to = "variable"
+  )
+
+usethis::use_data(notifications, overwrite = TRUE)
