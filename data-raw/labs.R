f750bc1e5fd4bd564e56490d89d273c973feea49
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-12-02 10:33:19 +0100

    Add labs data

--- data-raw/labs.R
+++ data-raw/labs.R
@@ -1,1 +1,29 @@
-readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=labs")
+raw <- readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=labs")
+
+raw_subset <- raw |> dplyr::select(
+  tidyselect::all_of(
+    c(
+      "country",
+      "country_code" = "iso3",
+      "g_whoregion",
+      "year",
+      "culture",
+      "lab_cul",
+      "lab_sm",
+      "smear",
+      "lab_xpert",
+      "xpert",
+      "m_wrd",
+      "m_wrd_tests_performed",
+      "m_wrd_tests_positive"
+    )
+  )
+)
+
+labs <- raw_subset |>
+  tidyr::pivot_longer(
+    cols = -c(country, country_code, g_whoregion, year),
+    names_to = "variable"
+  )
+
+usethis::use_data(labs, overwrite = TRUE)
