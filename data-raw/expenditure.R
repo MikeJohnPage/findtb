f34c2f0a8c4dcc288893974111a88c733a3393cc
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-12-02 10:33:11 +0100

    Add expenditure data

--- data-raw/expenditure.R
+++ data-raw/expenditure.R
@@ -1,1 +1,27 @@
-readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=expenditure_utilisation")
+raw <- readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=expenditure_utilisation")
+
+raw_subset <- raw |> dplyr::select(
+  tidyselect::all_of(
+    c(
+      "country",
+      "country_code" = "iso3",
+      "g_whoregion",
+      "year",
+      "rcvd_lab",
+      "rcvd_staff",
+      "rcvd_tot_domestic",
+      "rcvd_tot_gf",
+      "rcvd_tot_grnt",
+      "rcvd_tot_sources",
+      "rcvd_tot_usaid"
+    )
+  )
+)
+
+expenditure <- raw_subset |>
+  tidyr::pivot_longer(
+    cols = -c(country, country_code, g_whoregion, year),
+    names_to = "variable"
+  )
+
+usethis::use_data(expenditure, overwrite = TRUE)
