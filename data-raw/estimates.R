fb20dc355b46274215add8a0dcad3c9ac1dcce23
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-12-02 10:33:01 +0100

    Add estimates data

--- data-raw/estimates.R
+++ data-raw/estimates.R
@@ -1,1 +1,26 @@
-readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=estimates")
+raw <- readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=estimates")
+
+raw_subset <- raw |> dplyr::select(
+  tidyselect::all_of(
+    c(
+      "country",
+      "country_code" = "iso3",
+      "g_whoregion",
+      "year",
+      "c_cdr",
+      "c_newinc_100k",
+      "e_inc_100k",
+      "e_inc_num",
+      "e_mort_100k",
+      "e_pop_num"
+    )
+  )
+)
+
+estimtates <- raw_subset |>
+  tidyr::pivot_longer(
+    cols = -c(country, country_code, g_whoregion, year),
+    names_to = "variable"
+  )
+
+usethis::use_data(estimtates, overwrite = TRUE)
