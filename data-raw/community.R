b7f96c5c82986b2352f5ba56cf60e6609d58823a
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-12-02 10:32:54 +0100

    Add community data

--- data-raw/community.R
+++ data-raw/community.R
@@ -1,1 +1,27 @@
-readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=community")
+raw <- readr::read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=community")
+
+raw_subset <- raw |> dplyr::select(
+  tidyselect::all_of(
+    c(
+      "country",
+      "country_code" = "iso3",
+      "g_whoregion",
+      "year",
+      "bmu",
+      "bmu_community_impl",
+      "bmu_ref_data",
+      "bmu_rxsupport_data",
+      "bmu_rxsupport_data_coh",
+      "notified_ref",
+      "notified_ref_community"
+    )
+  )
+)
+
+community <- raw_subset |>
+  tidyr::pivot_longer(
+    cols = -c(country, country_code, g_whoregion, year),
+    names_to = "variable"
+  )
+
+usethis::use_data(community, overwrite = TRUE)
