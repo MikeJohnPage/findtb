fc24a269b6f1425cc8eebb5c476aee791bbc4198
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-12-02 10:21:20 +0100

    Add budget data

---
+++ data-raw/budget.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+
+raw <- read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=budget")
+
+raw_subset <- raw |> select(
+  all_of(
+    c(
+      "country",
+      "country_code" = "iso3",
+      "g_whoregion",
+      "year",
+      "budget_lab",
+      "budget_oth",
+      "budget_staff",
+      "cf_lab",
+      "cf_staff",
+      "cf_tot_domestic",
+      "cf_tot_gf",
+      "cf_tot_grnt",
+      "cf_tot_sources",
+      "cf_tot_usaid"
+    )
+  )
+)
+
+budget <- raw_subset |>
+  pivot_longer(
+    cols = -c(country, country_code, g_whoregion, year),
+    names_to = "variable"
+  )
+
+usethis::use_data(budget, overwrite = TRUE)
