f2cc67c84057e8c072cc2f9238d7d359bdc0db9c
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-12-04 12:29:10 +0100

    Remove startup message

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage(
-    paste0(
-      "findtb contains open source data.\n",
-      "View the terms of the license here: [insert link]"
-    )
-  )
-}
